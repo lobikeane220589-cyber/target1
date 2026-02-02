7a5b1d985b672d3309cc53761c9b1b865ec4806c
lobikeane220589-cyber <lobikeane220589@gmail.com>
2026-02-03 01:18:16 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { Problems } from './components/Problems'; // Repurposed as "Who Fits"
